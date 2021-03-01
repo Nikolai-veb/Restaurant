a2f0e1dd38f3a622bb7c42b22c629da042f8a67d
Nikloay <nik.vagin1995@mail.ru>
2021-03-01 23:08:47 +0300

    Craeted models Categories,Status,Products in app menu

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
+import datetime
 
-# Create your models here.
+from PIL import Image
+
+class Categories(models.Model):
+    name = models.CharField('Category', max_length=250)
+    slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('by_category', kwargs={"cat_slug": self.slug})
+
+    def __str__(self):
+        return self.name
+
+
+
+class Status(models.Model):
+    name = models.CharField('Status', max_length=250)
+    slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('by_status', kwargs={"stat_slug": self.slug})
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField("Product", max_length=250)
+    image = models.ImageField("Image", upload_to="products/%Y/%m/%d/", blank=True, null=True)
+    description =models.TextField("Description",)
+    category = models.ForeignKey(Categories, on_delete=models.PROTECT, null=True, related_name="product")
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, related_name="product")
+    slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
+    moderation = models.BooleanField(default=True)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["create"]
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={"prod_slug": self.slug})
+
+    def __str__(self):
+        return self.name
