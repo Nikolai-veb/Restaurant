c473949c04bf9ec3c0e0d91acb5901d8b42ee727
Nikloay <nik.vagin1995@mail.ru>
2021-03-02 00:10:25 +0300

    Chenged file admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories, Status, Products
+
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name")
+    list_filter = ("name",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name")
+    list_filter = ("name",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "get_image", "moderation", "price")
+    list_editable = ("moderation",)
+    list_display_links = ("id", "name")
+    list_filter = ("name", "description", "category", "status", "create", "price")
+    search_fields = ("name", "description")
+    prepopulated_fields = {"slug": ("name",)}
--- menu/models.py
+++ menu/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.urls import reverse
-import datetime
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
 
 from PIL import Image
 
 class Categories(models.Model):
     name = models.CharField('Category', max_length=250)
     slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
+   
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def get_absolute_url(self):
         return reverse('by_category', kwargs={"cat_slug": self.slug})
@@ -20,6 +25,10 @@ class Status(models.Model):
     name = models.CharField('Status', max_length=250)
     slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
 
+    class Meta:
+        verbose_name = "Status"
+        verbose_name_plural = "Status"
+
     def get_absolute_url(self):
         return reverse('by_status', kwargs={"stat_slug": self.slug})
 
@@ -29,16 +38,19 @@ class Status(models.Model):
 
 class Products(models.Model):
     name = models.CharField("Product", max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField("Image", upload_to="products/%Y/%m/%d/", blank=True, null=True)
     description =models.TextField("Description",)
     category = models.ForeignKey(Categories, on_delete=models.PROTECT, null=True, related_name="product")
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, related_name="product")
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, blank=True, related_name="product")
     slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
     moderation = models.BooleanField(default=True)
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
 
     class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
         ordering = ["create"]
 
     def get_absolute_url(self):
@@ -46,3 +58,12 @@ class Products(models.Model):
 
     def __str__(self):
         return self.name
+   
+    def get_image(self):
+        """The function of displaying pictures in the admin panel"""
+        if self.images:
+            return mark_safe(f'<img src={self.image.url} width="50" height="60">')
+        else:
+            return '(No found image)'
+    get_image.short_description = "Image"
+    get_image.allow_tags = True
