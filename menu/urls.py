f838723cb14646f3565650c94cf90ceea7af0879
Nikloay <nik.vagin1995@mail.ru>
2021-03-07 21:47:28 +0300

    Set up templates for app menu and home page

--- menu/urls.py
+++ menu/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page, name='home'),
+    path("menu/", views.MenuListView.as_view(), name='list_menu'),
+
 ]
--- menu/views.py
+++ menu/views.py
@@ -4,10 +4,18 @@ from .models import Categories, Status, Products
 
 
 def home_page(request):
-    menu = Products.objects.all()
-    return render(request, 'home_page.html', {menu: 'menu'})
+    categories = Categories.objects.all()
+    products = Products.objects.filter(moderation=True)
+    return render(request, 'home_page.html', {'products': products, 'categories': categories})
+
 
 class MenuListView(ListView):
     model = Products
     context_object_name = 'list_menu'
     template_name = 'menu/list_menu.html'
+    queryset = Products.objects.filter(moderation=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Categories.objects.all()
+        return context
